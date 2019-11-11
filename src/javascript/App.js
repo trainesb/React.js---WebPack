492df0baa0cb97d6958006897ca68c0cce8570fd
trainesb <37374492+trainesb@users.noreply.github.com>
2019-11-11 17:50:04 -0500

    Add files via upload

---
+++ src/javascript/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import Hello from "./components/container/Hello.jsx";
+
+class App extends Component {
+
+    render() {
+        return(<Hello />
+        );
+    }
+}
+
+export default App;
